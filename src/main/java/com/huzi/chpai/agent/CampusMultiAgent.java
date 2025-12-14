30bc9a6a914b2904e8956920eb7d5780ea141480
huzi <huzi@qq.com>
2025-12-14 15:53:53 +0800

    工具调用

--- src/main/java/com/huzi/chpai/agent/CampusMultiAgent.java
+++ src/main/java/com/huzi/chpai/agent/CampusMultiAgent.java
@@ -9,6 +9,7 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
@@ -94,6 +95,24 @@ public class CampusMultiAgent {
                 .chatResponse();
         String content = chatResponse.getResult().getOutput().getText();
         log.info("content: {}", content);
+        return content;
+    }
+
+    @Resource
+    private ToolCallback[] allTools;
+
+    public String doChatWithTools(String message, String chatId) {
+        ChatResponse response = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+
+                .advisors(new MyLoggerAdvisor())
+                .tools(allTools)
+                .call()
+                .chatResponse();
+        String content = response.getResult().getOutput().getText();
         return content;
     }
 }
---
+++ src/main/java/com/huzi/chpai/constant/FileConstant.java
@@ -0,0 +1,5 @@
+package com.huzi.chpai.constant;
+
+public interface FileConstant {
+    String FILE_SAVE_DIR = System.getProperty("user.dir") + "/tmp";
+}
